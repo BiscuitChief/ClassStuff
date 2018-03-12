f0642f06db84ef6480c84f5d9db26425534f51d9
BiscuitChief <scperkins81@gmail.com>
2018-03-12 15:59:04 -0400

    dish detail

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Dish } from '../shared/dish';
 
 const DISH = {
   name: 'Uthappizza',
@@ -55,6 +55,6 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
   }
 
-  dish = DISH;
-
+  @Input()
+  dish: Dish;
 }
